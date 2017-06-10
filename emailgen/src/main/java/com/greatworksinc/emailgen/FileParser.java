64933d04e20daa96ca98f9266736849300a05ff9
Simon Greene <simongreene02@gmail.com>
2017-06-10 11:06:23 -0500

    Began working on file parsing system.

---
+++ emailgen/src/main/java/com/greatworksinc/emailgen/FileParser.java
@@ -0,0 +1,54 @@
+package com.greatworksinc.emailgen;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.io.Resources;
+
+public class FileParser {
+	private static final Logger log = LoggerFactory.getLogger(FileParser.class);
+	
+	// buffering
+	public void bufferFile() {
+
+		try {
+			List<String> data = Files.readAllLines(getPath("first200sort.txt"));
+			System.out.println(data.toString());
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void streamFilePerLine() throws FileNotFoundException {
+		BufferedReader br = new BufferedReader(new FileReader(getFile("first200sort.txt")));
+		// int count = 0;
+		// br.readLine(); // stream file each line as an atomit unit.
+	}
+
+	public static File getFile(String name) {
+		try {
+			return new File(Resources.getResource(name).toURI());
+		} catch (URISyntaxException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static Path getPath(String name) {
+		try {
+			return Paths.get(Resources.getResource(name).toURI());
+		} catch (URISyntaxException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
---
+++ emailgen/src/test/java/com/greatworksinc/emailgen/FileParserTest.java
@@ -0,0 +1,14 @@
+package com.greatworksinc.emailgen;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FileParserTest {
+
+	@Test
+	public void test() {
+		new FileParser().bufferFile();
+	}
+
+}
